630c08ec98addc099bbf8d6a7f072911b9f8355d
Markus Igeland <markusigeland@gmail.com>
2019-12-30 19:35:04 +0100

    Some small cleanup and planning
    Moved code out of main.py into toolkit.py. The functions are just
    imported and ran from main.py, just like earlier, but now we can
    separate loading of plugins from running of the bot easier.
    
    Added a small script to help create a simple plugin.
    Added a plugin template.

---
+++ create_plugin.py
@@ -0,0 +1,34 @@
+import os
+from string import Template
+
+plugin_name=input('Select a name for the plugin: ')
+plugin_priority=input('Which priority should this plugin have? (default: 0)')
+plugin_allow_others=input('Should this plugin block other plugins from running? [y/N] ')
+
+if not plugin_priority:
+    plugin_priority = 0
+
+plugin_allow_others = 'y' == plugin_allow_others or \
+    'Y' == plugin_allow_others or \
+    'yes' == plugin_allow_others or \
+    'YES' == plugin_allow_others or \
+    'Yes' == plugin_allow_others
+
+if plugin_allow_others:
+    plugin_allow_others = 'True'
+else:
+    plugin_allow_others = 'False'
+
+with open('plugin.template', 'r') as plugin_template:
+    plugin_template = ''.join(plugin_template.readlines()) + '\n\n'
+    template = Template(plugin_template)
+    plugin = template.substitute(dict(
+        name=plugin_name,
+        priority=plugin_priority,
+        allow_others=plugin_allow_others
+    ))
+
+    os.mkdir('plugins/{0}' . format(plugin_name))
+    with open('plugins/{0}/main.py' . format(plugin_name), 'w+') as plugin_file:
+        plugin_file.write(plugin)
+        plugin_file.close()
